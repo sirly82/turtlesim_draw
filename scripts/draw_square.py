a9cec0badbe74af93de195270472936853f072e2
sirly82 <srziafidah@gmail.com>
2025-12-12 07:54:07 +0700

    update fix code and launch

--- scripts/draw_square.py
+++ scripts/draw_square.py
@@ -2,34 +2,47 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+def callback_pose(msg):
+    rospy.loginfo("Pose: x=%f y=%f theta=%f", msg.x, msg.y, msg.theta)
 
 def draw_square():
-    rospy.init_node('draw_square', anonymous=True)
+    rospy.init_node('draw_square')
+
     pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
-    rate = rospy.Rate(1)  # 1 Hz
+    sub = rospy.Subscriber('turtle1/pose', Pose, callback_pose)
+
+    rospy.sleep(1)
+
     move_cmd = Twist()
 
-    for _ in range(5):  # Draw a square
-        # Move forward
-        move_cmd.linear.x = 2.0
+    rate = rospy.Rate(10)  # 10 Hz
+
+    while not rospy.is_shutdown():
+        # --- 1 rectange ---
+        for _ in range(4):
+            # Move forward
+            move_cmd.linear.x = 2.0
+            move_cmd.angular.z = 0.0
+            pub.publish(move_cmd)
+            rospy.sleep(2)
+
+            # Turn 90 deg
+            move_cmd.linear.x = 0.0
+            move_cmd.angular.z = 1.57
+            pub.publish(move_cmd)
+            rospy.sleep(1.5)
+
+        # Stop for 0.5s before next square
+        move_cmd.linear.x = 0.0
         move_cmd.angular.z = 0.0
         pub.publish(move_cmd)
-        rospy.sleep(2)
+        rospy.sleep(0.5)
 
-        # Turn 90 degrees
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = 1.56  # Approx 90 degrees
-        pub.publish(move_cmd)
-        rospy.sleep(2)
+        rospy.loginfo("Kotak selesai → mulai kotak berikutnya")
 
-    # Stop the turtle
-    move_cmd.linear.x = 0.0
-    move_cmd.angular.z = 0.0
-    pub.publish(move_cmd)
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        draw_square()
-    except rospy.ROSInterruptException:
-        pass
-
+    draw_square()
